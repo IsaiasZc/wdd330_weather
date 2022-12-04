f4c836fba2af86b0a0c079fdc5bf4028f858ef97
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2022-12-03 23:56:52 -0500

    btn views done

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,83 +1,49 @@
+import CitiesView from "./views/CitiesView.js";
+import ForeCastView from "./views/ForeCastView.js";
+import HomeView from "./views/HomeView.js";
+import LocationView from "./views/LocationView.js";
+
 /**
  * A helper to manipulate the DOM
  */
-
 export default class Utilities {
+
   /**
    * 
    * @param {selector} parent selector for the view
    */
-  constructor(parent) {
-    this.parent = $(parent);
+  constructor(parent, btns) {
+    this.parent = this.$(parent)[0];
+    this.home = new HomeView("home");
+    this.forecast = new ForeCastView("forecast");
+    this.cities = new CitiesView("cities");
+    this.location = new LocationView("location");
+
+  };
+
+  showLocation() {
+    const view = ""
+    this.showMainView(view)
   }
 
-  /**
-   * create a weather card based on the given object
-   * @param {object} wth object with all the information
-   * @returns node
-   */
-  createCurrentCard(wth) {
-    const card = cn("div");
-    card.className = "currentCard p-4 c-mainBlue d-flex";
-    card.innerHTML = `
-      <section class="card_main d-flex gap-4 flex-column w-100 p-3">
-        <div class="card_main_head d-flex justify-content-between">
-          <h5>
-            <i class="bi bi-geo-alt"></i>
-            <span class="b-700" id="card-place">${wth.name}</span>
-          </h5>
-          <p class="m-0">Today <span id="card-time">${timeFormat()}</span></p>
-        </div>
-        <div class="card_main_tmp d-flex flex-column align-items-center">
-          <span class="currentCard-temp lh-1">
-            ${Math.round(wth.main.temp)}°
-          </span>
-          <span>
-            ${wth.weather[0].description}
-          </span>
-        </div>
-        <div class="card_main_more d-flex justify-content-between">
-          <article>
-            <i class="bi bi-wind"></i>
-            <span>${wth.main.pressure}hpa</span>
-          </article>
-          <article>
-            <i class="bi bi-droplet"></i>
-            <span>${wth.main.humidity}%</span>
-          </article>
-          <article>
-            <i class="bi bi-cloud-haze2-fill"></i>
-            <span>${Math.round(wth.wind.speed)}km/h</span>
-          </article>
-        </div>
-      </section>
+  showCities() {
+    const view = ""
+    this.showMainView(view)
+  }
 
-      <section class="card_tmp_detailed py-2 rounded-3 w-100">
-        <h5 class="ps-3">temperature</h5>
-        ${testInfo()}
-
-      </section>
-    `
-    return card
+  showForecast() {
+    const view = ""
+    this.showMainView(view)
   }
 
   /**
    * 
-   * @param {data} data prepare the home view
-   * @returns the home view
+   * @param {object} data information for the main card
    */
-  prepareHome(data, details=[]) {
-
-    const mainCard = this.createCurrentCard(data);
-    const detailsC = this.createMdetailCards(details);
-    const homeView = cn("section");
-    homeView.className = "home-view container-sm";
-
-    homeView.appendChild(mainCard);
-    homeView.appendChild(detailsC);
-
-    return homeView;
-  }
+  showHome(data, details=[]) {
+    const view = this.home.prepareHome(data, details);
+    this.showMainView(view)
+  };
 
   /**
    * Insert the given view
@@ -87,52 +53,13 @@ export default class Utilities {
     this.parent.innerHTML = "";
 
     this.parent.appendChild(nodo);
-  }
-
-
-  /**
-   * 
-   * @param {object} data information for the main card
-   */
-  showHome(data, details=[]) {
-    const view = this.prepareHome(data, details);
-    this.showMainView(view)
-  }
-
-
-  createMdetailCards(arr) {
-    const details = cn("div");
-    details.className = "details";
-
-    arr.forEach( data => details.appendChild(this._createMDetailCard(data)))
-    
-    return details
-  }
-
-
-  _createMDetailCard(obj) {
-    const card = cn("div");
-    const {title, descrip, value, imgPath} = obj;
-
-    card.innerHTML = `
-    <div class="data">
-      <h2>${title}</h2>
-      <span>${descrip}</span>
-      <span>${value}</span>
-    </div>
-    <div class="image">
-    <img src="${imgPath}" alt="${title}">
-    </div>
-    `
-
-    return card
-  }
+  };
 
   _classesLoading() {
     this.parent.classList.toggle("d-flex");
     this.parent.classList.toggle("justify-content-center");
     this.parent.classList.toggle("align-items-center");
-  }
+  };
 
   setLoadingView() {
     // this._classesLoading();
@@ -141,10 +68,12 @@ export default class Utilities {
       <span class="visually-hidden">Loading...</span>
     </div>
     `
-  }
+  };
+
+  $(select) {
+    return [...document.querySelectorAll(select)] || console.error("El elemento indicado no existe en el DOM");
+  };
 }
-
-
 
 /**
  * select an existing Element
@@ -153,46 +82,4 @@ export default class Utilities {
  */
 function $(select) {
   return document.querySelector(select) || console.error("El elemento indicado no existe en el DOM");
-}
-
-/**
- * Create a new element
- * @param {string} elem element to be created
- * @returns a node
- */
-function cn(elem) {
-  return document.createElement(elem)
-}
-
-function timeFormat(time) {
-  let newTime = time ? new Date(time) : new Date();
-
-  return newTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-}
-
-
-function testInfo() {
-  return  `
-  <div class="line" style="height: 100px; width: 100%;">
-
-  </div>
-  <div class="div d-flex justify-content-between text-center">
-    <p>
-      Morning
-      <span>26°</span>
-    </p>
-    <p>
-      Afternon
-      <span>24°</span>
-    </p>
-    <p>
-      Evening
-      <span>28°</span>
-    </p>
-    <p>
-      Night
-      <span>20°</span>
-    </p>
-  </div>
-  `
 }
